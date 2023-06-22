abb960831a8f04616f754a040d24c9714368f386
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-22 13:23:21 +1000

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,3 +1,11 @@
 <?php
 namespace skyss0fly\Bug;
 
+use pocketmine\Player\Player;
+use pocketmine\Plugin\PluginBase;
+
+class main extends PluginBase implements Listener{
+public function onLoad(){
+$config = $this->get(config)
+  
+
